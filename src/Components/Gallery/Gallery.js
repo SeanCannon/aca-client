47186558f31c68548b9ff8c232214494915a6f86
Sean Cannon <alienwebguy@gmail.com>
2020-04-13 09:32:51 -0700

    aca-client-15 - fix infinite scroll

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -17,12 +17,12 @@ import {
   Loader
 } from 'semantic-ui-react';
 
+import InfiniteScroll from 'react-infinite-scroll-component';
+
 import { shuffleMutate } from '../../utils';
-import { useInfiniteScroll, useLazyLoading } from '../../customHooks';
 import { Api, ArtSvc } from '../../api';
 
 const Gallery = () => {
-  const [debounce, setDebounce]                 = useState(false);
   const [isLoading, setIsLoading]               = useState(true);
   const [isLoadingMore, setIsLoadingMore]       = useState(true);
   const [searchParams, setSearchParams]         = useState({});
@@ -30,44 +30,21 @@ const Gallery = () => {
   const [galleryItemIds, setGalleryItemIds]     = useState([]);
   const [galleryItems, setGalleryItems]         = useState([]);
   const [galleryItemCount, setGalleryItemCount] = useState(0);
-  const [pageNum, setPageNum]                   = useState(1);
+  const [pageNum, setPageNum]                   = useState(0);
 
-  const bottomBoundaryRef = useRef(null);
+  const GALLERY_ITEMS_PER_FETCH = 20;
 
-  const nextPageDebounced = () => {
-    if (debounce) {
-      return;
-    }
-    setDebounce(true);
-    setTimeout(() => setDebounce(false), 500);
-
-    setPageNum(pageNum + 1);
-  };
-
-  useEffect(() => {
-    if (!isLoading) {
-      setIsLoadingMore(true);
-    }
-    Promise.all(galleryItemIds.slice(pageNum * 16 - 16, pageNum * 16 + 1).map(id => {
+  const fetchMoreItems = () => {
+    const page = pageNum + 1;
+    console.log('page = ', page)
+    console.log(galleryItemIds.slice(page  * GALLERY_ITEMS_PER_FETCH - GALLERY_ITEMS_PER_FETCH, page * GALLERY_ITEMS_PER_FETCH))
+    return Promise.all(galleryItemIds.slice(page  * GALLERY_ITEMS_PER_FETCH - GALLERY_ITEMS_PER_FETCH, page * GALLERY_ITEMS_PER_FETCH).map(id => {
       return ArtSvc.getItemById(Api)({ strategy, id });
     }))
-      .then(images => {
-        setGalleryItems([ ...galleryItems, ...images]);
-
-        if (isLoading) {
-          setIsLoading(false);
-        }
-
-        setIsLoadingMore(true);
-      })
-      .catch(e => {
-        console.log({ e });
-        return e;
-      });
-  }, [pageNum]);
-
-  useLazyLoading('.ui.card', galleryItems);
-  useInfiniteScroll(bottomBoundaryRef, nextPageDebounced);
+      .then(images => setGalleryItems([ ...galleryItems, ...images]))
+      .then(() => setPageNum(page))
+      .catch(console.error);
+  };
 
   useEffect(() => {
     setIsLoading(true);
@@ -80,47 +57,53 @@ const Gallery = () => {
       });
   }, [searchParams, strategy]);
 
+  useEffect(() => {
+    fetchMoreItems();
+  }, [galleryItemIds]);
+
   return (
     <Segment id="gallery" style={{ padding : '8em 0em' }} vertical>
       <Container>
         <Header as="h3" style={{ fontSize : '2em' }}>
           Gallery
         </Header>
-        {
-          isLoading ? <Loader content='Loading' /> : (
-            <Grid>
-              {
-                galleryItems.map(({ id, title, primaryImageSmall }) => (
-                  <Grid.Column key={id} mobile={16} tablet={8} computer={4}>
-                    <Card>
-                      <Image src={primaryImageSmall} wrapped ui={false} />
-                      <Card.Content>
-                        <Card.Header>{title}</Card.Header>
-                        <Card.Meta>Courtesy of MET</Card.Meta>
-                        {/*<Card.Description>*/}
-                        {/*Daniel is a comedian living in Nashville.*/}
-                        {/*</Card.Description>*/}
-                      </Card.Content>
-                      {/*<Card.Content extra>*/}
-                      {/*<a>*/}
-                      {/*<Icon name='user' />*/}
-                      {/*10 Friends*/}
-                      {/*</a>*/}
-                      {/*</Card.Content>*/}
-                    </Card>
-                  </Grid.Column>
-                ))
-              }
-            </Grid>
-          )
-        }
+        <InfiniteScroll
+          dataLength={galleryItems.length}
+          next={fetchMoreItems}
+          hasMore={galleryItems.length < galleryItemCount}
+          loader={<Loader content='Loading' />}
+          endMessage={<h5>End of this gallery</h5>}
+        >
+          <Grid>
+            {
+              galleryItems.map(({ id, title, primaryImageSmall }) => (
+                <Grid.Column key={id} mobile={16} tablet={8} computer={4}>
+                  <Card>
+                    <Image
+                      src={primaryImageSmall}
+                      wrapped
+                      size="large"
+                    />
+                    <Card.Content>
+                      <Card.Header>{title}</Card.Header>
+                      <Card.Meta>Courtesy of MET</Card.Meta>
+                      {/*<Card.Description>*/}
+                      {/*Daniel is a comedian living in Nashville.*/}
+                      {/*</Card.Description>*/}
+                    </Card.Content>
+                    {/*<Card.Content extra>*/}
+                    {/*<a>*/}
+                    {/*<Icon name='user' />*/}
+                    {/*10 Friends*/}
+                    {/*</a>*/}
+                    {/*</Card.Content>*/}
+                  </Card>
+                </Grid.Column>
+              ))
+            }
+          </Grid>
+        </InfiniteScroll>
 
-        { isLoading ? <Loader content='Loading' /> : null }
-
-        <div
-          id="page-bottom-boundary"
-          ref={bottomBoundaryRef}
-        />
       </Container>
     </Segment>
   );
