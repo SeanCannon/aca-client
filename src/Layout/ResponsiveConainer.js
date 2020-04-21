5baf7f4179eb87e151a5566c627fbe638f8beba7
Sean Cannon <alienwebguy@gmail.com>
2020-04-21 09:31:56 -0700

    aca-client-43 - add mobile menu

--- src/Layout/ResponsiveConainer.js
+++ src/Layout/ResponsiveConainer.js
@@ -13,12 +13,62 @@ import Hero from '../Components/Hero/Hero';
 import UploadModal from '../Components/ImageModal/ImageModal';
 import TutorialModal from '../Components/TutorialModal/TutorialModal';
 
-const siteName = 'Animal Crossing Art';
-
 const getWidth = () => {
   const isSSR = typeof window === 'undefined';
 
   return isSSR ? Responsive.onlyTablet.minWidth : window.innerWidth;
+};
+
+const MenuContainer = ({
+  handleToggleUploadModal,
+  handleToggleTutorialModal,
+  hideSidebar = () => {}
+}) => (
+  <Container>
+    <Menu.Item as="a" href="/">
+      Home
+    </Menu.Item>
+    <Menu.Item as="a" target="_blank" href="https://discord.gg/mBMsHvN">
+      Discord
+    </Menu.Item>
+    <Menu.Item as="a" target="_blank" href="https://www.facebook.com/AnimalCrossingArt-110478317288315">
+      Facebook
+    </Menu.Item>
+    <Menu.Item as="a" target="_blank" href="https://twitter.com/crossingart">
+      Twitter
+    </Menu.Item>
+    <Menu.Item as="a" target="_blank" href="https://www.instagram.com/animalcrossingartdotcom">
+      Instagram
+    </Menu.Item>
+    <Menu.Menu position="right">
+      <Menu.Item
+        name="uploadImage"
+        onClick={() => {
+          handleToggleUploadModal();
+          hideSidebar();
+        }}
+      >
+        <Icon name="upload" /> Upload Image
+      </Menu.Item>
+    </Menu.Menu>
+    <Menu.Menu position="right">
+      <Menu.Item
+        name="tutorial"
+        onClick={() => {
+          handleToggleTutorialModal();
+          hideSidebar();
+        }}
+      >
+        <Icon name="help" /> Tutorial
+      </Menu.Item>
+    </Menu.Menu>
+  </Container>
+);
+
+MenuContainer.propTypes = {
+  handleToggleUploadModal : PropTypes.func,
+  handleToggleTutorialModal : PropTypes.func,
+  hideSidebar : PropTypes.func
 };
 
 const DesktopContainer = ({ children }) => {
@@ -54,39 +104,10 @@ const DesktopContainer = ({ children }) => {
             secondary={!fixed}
             size="large"
           >
-            <Container>
-              <Menu.Item as="a" href="/">
-                Home
-              </Menu.Item>
-              <Menu.Item as="a" target="_blank" href="https://discord.gg/mBMsHvN">
-                Discord
-              </Menu.Item>
-              <Menu.Item as="a" target="_blank" href="https://www.facebook.com/AnimalCrossingArt-110478317288315">
-                Facebook
-              </Menu.Item>
-              <Menu.Item as="a" target="_blank" href="https://twitter.com/crossingart">
-                Twitter
-              </Menu.Item>
-              <Menu.Item as="a" target="_blank" href="https://www.instagram.com/animalcrossingartdotcom">
-                Instagram
-              </Menu.Item>
-              <Menu.Menu position="right">
-                <Menu.Item
-                  name="uploadImage"
-                  onClick={handleToggleUploadModal}
-                >
-                  <Icon name="upload" /> Upload Image
-                </Menu.Item>
-              </Menu.Menu>
-              <Menu.Menu position="right">
-                <Menu.Item
-                  name="tutorial"
-                  onClick={handleToggleTutorialModal}
-                >
-                  <Icon name="help" /> Tutorial
-                </Menu.Item>
-              </Menu.Menu>
-            </Container>
+            <MenuContainer
+              handleToggleUploadModal={handleToggleUploadModal}
+              handleToggleTutorialModal={handleToggleTutorialModal}
+            />
           </Menu>
           <Hero />
         </Segment>
@@ -115,9 +136,13 @@ DesktopContainer.propTypes = {
 
 const MobileContainer = ({ children }) => {
   const [sidebarOpened, setSidebarOpened] = useState(false);
+  const [openUpload, setOpenUpload] = useState(false);
+  const [openTutorial, setOpenTutorial] = useState(false);
 
   const hideSidebar = () => setSidebarOpened(false);
   const showSidebar = () => setSidebarOpened(true);
+  const handleToggleUploadModal = () => setOpenUpload(!openUpload);
+  const handleToggleTutorialModal = () => setOpenTutorial(!openTutorial);
 
   return (
     <Responsive
@@ -133,9 +158,11 @@ const MobileContainer = ({ children }) => {
         vertical
         visible={sidebarOpened}
       >
-        <Menu.Item as="a" active>
-          {siteName}
-        </Menu.Item>
+        <MenuContainer
+          handleToggleUploadModal={handleToggleUploadModal}
+          handleToggleTutorialModal={handleToggleTutorialModal}
+          hideSidebar={hideSidebar}
+        />
       </Sidebar>
 
       <Sidebar.Pusher dimmed={sidebarOpened}>
@@ -159,6 +186,19 @@ const MobileContainer = ({ children }) => {
         </Segment>
 
         {children}
+
+        { openUpload ? (
+          <UploadModal
+            onClose={handleToggleUploadModal}
+          />
+        ) : null }
+
+        { openTutorial ? (
+          <TutorialModal
+            onClose={handleToggleTutorialModal}
+          />
+        ) : null }
+
       </Sidebar.Pusher>
     </Responsive>
   );
